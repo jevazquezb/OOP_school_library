25311d5a8ad7fea71362da866392d822480336ad
jevazquezb <je.vazquezb@gmail.com>
2022-08-23 18:03:15 -0500

    Update tests

--- tests.rb
+++ tests.rb
@@ -1,32 +1,16 @@
 require './person'
 require './student'
 require './teacher'
+require './concrete_decorators'
 
-mairy = Person.new(15, 'Mairy')
-p mairy
-p mairy.can_use_services?
+person = Person.new(22, 'maximilianus')
+p person
+p person.correct_name
 
-mairy = Person.new(15, 'Mairy', parent_permission: false)
-p mairy
-p mairy.can_use_services?
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson
+p capitalizedPerson.correct_name
 
-mairy = Person.new(18, 'Mairy', parent_permission: false)
-p mairy
-p mairy.can_use_services?
-
-roberto = Student.new(601, 17, 'Roberto')
-p roberto
-p roberto.play_hooky
-p roberto.can_use_services?
-
-roberto = Student.new(601, 17, 'Roberto', parent_permission: false)
-p roberto
-p roberto.can_use_services?
-
-roberto = Student.new(601, 20, 'Roberto', parent_permission: false)
-p roberto
-p roberto.can_use_services?
-
-teacher = Teacher.new('Physics', 43)
-p teacher
-p teacher.can_use_services?
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson
+p capitalizedTrimmedPerson.correct_name
