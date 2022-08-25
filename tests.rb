909c39997d35e913fdce5397b3a03f2e288a29a9
jevazquezb <je.vazquezb@gmail.com>
2022-08-24 20:26:39 -0500

    Fix linters bugs

--- tests.rb
+++ tests.rb
@@ -6,7 +6,7 @@ require './book'
 
 p 'Student-Classroom tests'
 p pedro = Student.new(501, 19, 'Pedro Salas')
-p laura = Student.new(103, 21, 'Laura Ponce' )
+p laura = Student.new(103, 21, 'Laura Ponce')
 p mairy = Student.new(207, 20, 'Mairy Calderón')
 
 p class619 = Classroom.new(101)
@@ -44,7 +44,7 @@ p class619.students.count
 p class619.students.last.name
 p class619.students.last.age
 
-p "Many-to-many tests"
+p 'Many-to-many tests'
 p miguel = Person.new('31', 'Miguel')
 p julio = Person.new('29', 'Julio')
 p manu = Person.new('35', 'Manuel')
@@ -52,22 +52,22 @@ p manu = Person.new('35', 'Manuel')
 p lab = Book.new('The Labyrinth of Solitude', 'Octavio Paz')
 p termo = Book.new('Thermodynamics: An Engineering Approach', 'Yunus Cengel')
 
-p rental_1 = Rental.new('2022-06-25', lab, miguel)
-p rental_2 = Rental.new('2022-08-13', lab, julio)
+p Rental.new('2022-06-25', lab, miguel)
+p Rental.new('2022-08-13', lab, julio)
 
 p miguel.rentals.count
-p miguel.rentals.map { |rental| rental.date }
+p miguel.rentals.map(&:date)
 p termo.rentals.count
 p lab.rentals.count
-p lab.rentals.map { |rental| rental.book.author}
-p lab.rentals.map { |rental| rental.person.name}
+p(lab.rentals.map { |rental| rental.book.author })
+p(lab.rentals.map { |rental| rental.person.name })
 
-p rental_3 = Rental.new('2022-07-18', termo, julio)
-p rental_4 = Rental.new('2022-08-04', lab, manu)
+p Rental.new('2022-07-18', termo, julio)
+p Rental.new('2022-08-04', lab, manu)
 
 p julio.rentals.count
-p julio.rentals.map { |rental| rental.date }
+p julio.rentals.map(&:date)
 p termo.rentals.count
-p termo.rentals.map { |rental| rental.person.name }
+p(termo.rentals.map { |rental| rental.person.name })
 p lab.rentals.count
-p lab.rentals.map {|rental| rental.person.name }
+p(lab.rentals.map { |rental| rental.person.name })
