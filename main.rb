6156d196709a2e928f6c1f86c9a091b487ce76ba
jevazquezb <je.vazquezb@gmail.com>
2022-08-26 00:19:44 -0500

    Remove options and place them in App class

--- main.rb
+++ main.rb
@@ -1,50 +1,25 @@
 require './app'
 
 def main
-  app = App.new()
-
+  app = App.new
   puts 'Welcome to School Library App!'
 
-  repeat = true
-  arr_books = []
-  arr_people = []
-  arr_rentals = []
-
-  while repeat
-    msg = "\nPlease choose an option by entering a number:
+  while true
+    puts "\nPlease choose an option by entering a number:
     1 - List all books.
     2 - List all people.
     3 - Create a person.
     4 - Create a book.
     5 - Create a rental.
     6 - List all rentals for a given person id.
-    7 - Exit.";
+    7 - Exit."
 
-    puts msg
     print "\nOption: "
     opt = gets.chomp.to_i
 
-    case opt
-    when 1
-      app.list_books(arr_books)                
-    when 2
-      app.list_people(arr_people)    
-    when 3
-      person = app.create_person()
-      arr_people.push(person) if person != nil
-    when 4
-      book = app.create_book()
-      arr_books.push(book) if book != nil
-    when 5
-      rental = app.create_rental(arr_books, arr_people)
-      arr_rentals.push(rental) if rental != nil
-    when 6
-      app.list_rentals(arr_rentals)
-    else
-      puts "Thank you for using this app!"
-      return
-    end
+    opt == 7 ? break : app.select_opt(opt)
   end
+  puts 'Thank you for using this app!'
 end
 
-main()
+main
